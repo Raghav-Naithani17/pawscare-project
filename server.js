8d6a4dff15129284b551f9e50ed832d5ada52135
Raghav <raghavnaithani173@gmail.com>
2026-04-06 12:01:57 +0530

    fix env loading properly

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 /* DB CONNECT */
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("✅ MongoDB Connected Successfully");
 })
